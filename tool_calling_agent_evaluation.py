362815cdd5c37034309d3e12eb2224f6cd108c1f
arthurxin <xinhongyu731@icloud.com>
2025-07-23 12:24:19 +0800

    fix adaptor for Openai tool calling

--- tool_calling_agent_evaluation.py
+++ tool_calling_agent_evaluation.py
@@ -1,6 +1,12 @@
 from llama_index.llms.openai import OpenAI
+from llama_index.llms.openai_like import OpenAILike
 from llama_index.llms.anthropic import Anthropic
 from adapters.llama_index_agent_adapter import LLamaIndexAgentFactory
+from adapters.openai_agent_adapter import OpenAIAgentFactory
+from openai import AsyncOpenAI
+from agents import OpenAIChatCompletionsModel
+
+
 import asyncio
 import json
 from evaluation import evaluate 
@@ -8,11 +14,26 @@ from utils.model_loader import load_model_config
 
 ground_truths = json.load(open("./scenarios/ground_truths.json"))
 
-async def evaluate_gpt_4o():
-    model_config = load_model_config("gpt-4o")
-    model = OpenAI(**model_config)
+async def evaluate_deepseek_v3():
+    model_config = load_model_config("qwen3")
+    model = OpenAILike(
+        model=model_config['model_id'],
+        api_base=model_config['base_url'],
+        api_key=model_config['api_key'],
+        is_function_calling_model=True,
+        is_chat_model=True
+    )
     agent_factory = LLamaIndexAgentFactory(model)
-    await evaluate(agent_factory, ground_truths, "./results/tool_calling_agent_gpt_4o.json")
+    await evaluate(agent_factory, ground_truths, "./results/tool_calling_agent_deepseek_v3.json")
+
+async def evaluate_qwen3():
+    model_config = load_model_config("qwen3")
+    model = OpenAIChatCompletionsModel(
+        model=model_config['model_id'], 
+        openai_client=AsyncOpenAI(base_url=model_config['base_url'], api_key=model_config['api_key'])
+    )
+    agent_factory = OpenAIAgentFactory(model)
+    await evaluate(agent_factory, ground_truths, "./results/tool_calling_agent_qwen3.json")
 
 async def evaluate_claude_3_7_sonnet():
     model_config = load_model_config("claude-3-7-sonnet-20240229")
@@ -22,5 +43,5 @@ async def evaluate_claude_3_7_sonnet():
 
 
 if __name__ == "__main__":
-    asyncio.run(evaluate_gpt_4o())
+    asyncio.run(evaluate_qwen3())
     # asyncio.run(evaluate_claude_3_7_sonnet())
